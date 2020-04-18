441e13a580914020c687d793b196721701c8d87f
Niccoryan0 <niccoryan0@gmail.com>
2020-04-18 14:31:32 -0700

    made a super basic scaffold and made the files we think well need

---
+++ gamecontroller.js
@@ -0,0 +1,1 @@
+// This will actually take inputs from the user, will get info from gameplay.js and pass it through necessary logic
---
+++ monsters.js
@@ -0,0 +1,7 @@
+// This is for constructor functions for monsters, abilities, all stats and methods related to monsters
+
+
+// TODO: Class for monsters, include stats
+
+
+// TODO: Class for abilities, tageting and affects of attack
