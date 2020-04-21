535deeb8ac675758b90b4615352861466b349de8
Niccoryan0 <niccoryan0@gmail.com>
2020-04-21 14:26:29 -0700

    styling on all pages, some changes to game screen, monster rendering on home, should have committed sooner

--- apps/gamemanager.js
+++ apps/gamemanager.js
@@ -1,9 +1,20 @@
 'use strict';
+/* GLOBAL: getRandomMonster */
 // Initialize data for monsters, save/load data for monsters, handle events outside of game function
 // THIS WILL BE THE SECOND FILE THAT GETS RUN ONLY ON THE HOMEPAGE
+var krapkenDesc = 'A cross between a Japanese Kappa and a Kraken. This monstrous beast has the upper half of a human like turtle creature, the Kappa, with the Eldritch abomination that is it\'s Kraken lower half. Krapken enjoys hanging out in the water, with only his top half exposed, in order to lure in unsuspecting victims to pull them under with him tentacles.';
+
+var userMonster;
+if (localStorage.getItem('userMonster')){
+  userMonster = localStorage.getItem(JSON.parse('userMonster'));
+} else {
+  // userMonster = getRandomMonster();
+  userMonster = {name : 'Krapken', attack : 30, defense: 30, speed: 30, description: krapkenDesc, imgSrc : 'assets/sprites/Krapken_160px_transparent.png'};
+}
 
 // Array
-var userMonster;
+
+
 
 // Gets a random monster from the Monster array
 
@@ -15,7 +26,7 @@ var userMonster;
 function renderUserSpriteHomepage(){
   var target = document.getElementById('picTray');
 
-  var userImgSrc = userMonster.imgSrc; // CHECK THIS LINE WITH BADE!!!
+  var userImgSrc = userMonster.imgSrc;
   var userImgEl = document.createElement('img');
   userImgEl.src = userImgSrc;
   userImgEl.height = 80;
@@ -23,20 +34,29 @@ function renderUserSpriteHomepage(){
 
   var userMonsterName = document.getElementById('userMonsterName');
   userMonsterName.textContent = userMonster.name;
-  var userMonsterStats = document.getElementById('userMonsterStats');
-  userMonsterStats.textContent = userMonster.stats
-  var userMonsterdesc = 
-};
+  var userMonsterAtk = document.getElementById('userMonsterAtk');
+  userMonsterAtk.textContent = 'Attack: ' + userMonster.attack;
+  var userMonsterDef = document.getElementById('userMonsterDef');
+  userMonsterDef.textContent = 'Defense: ' + userMonster.defense;
+  var userMonsterSpd = document.getElementById('userMonsterSpd');
+  userMonsterSpd.textContent = 'Speed: ' + userMonster.speed;
+  var userMonsterDesc = document.getElementById('userMonsterDescription');
+  userMonsterDesc.textContent = userMonster.description;
+
+
+}
+renderUserSpriteHomepage();
 
 var resetButton = document.getElementById('resetButton');
 resetButton.addEventListener('click', function(){
-  if (!localStorage.getItem('userMonster')){
-    localStorage.clear();
-    location.reload();
-  }
+  localStorage.clear();
+
   userMonster = getRandomMonster();
   var userMonsterStringy = JSON.stringify(userMonster);
   localStorage.setItem('userMonster', userMonsterStringy);
+
+  location.reload();
+
 });
 
 var startButton = document.getElementById('startButton');
--- apps/gameview.js
+++ apps/gameview.js
@@ -2,34 +2,16 @@
 'use strict';
 // TODO: Maybe get userMonster from localStorage, talk to Bade about file order
 
-<<<<<<< HEAD
 var userImgPath = userMonster.imgSrc; // CHECK THIS LINE WITH BADE!!!
 
 
 // TODO: Pull monster from local storage for user monster
-=======
-var userImgSrc = userMonster.imgSrc; // CHECK THIS LINE WITH BADE!!!
-
-// TODO: Pull monster from local storage for user monster
-
-
-function renderUserSpriteHomepage(){
-  var target = document.getElementById('picTray');
-
-  var userImgEl = document.createElement('img');
-  userImgEl.src = userimgSrc;
-  userImgEl.height = 80;
-  target.appendChild(userImgEl);
->>>>>>> 4690e26bf0e45419497dc11e82b410f5d81c4561
 
 
 function renderBattleSprites(enemy){
   // THIS IS TO RENDER THE USERS SPRITE FIRST
   var userTarget = document.getElementById('userBattlePosition');
-<<<<<<< HEAD
   var userImgSrc = userMonster.imgSrc; // CHECK THIS LINE WITH BADE!!!
-=======
->>>>>>> 4690e26bf0e45419497dc11e82b410f5d81c4561
   var userImgEl = document.createElement('img');
   userImgEl.src = userImgSrc;
   userImgEl.height = 80;
@@ -38,11 +20,7 @@ function renderBattleSprites(enemy){
   // THIS RENDERS THE ENEMY SPRITE
   var enemyTarget = document.getElementById('enemyBattlePosition');
   var enemyMonsterImg = document.createElement('img');
-<<<<<<< HEAD
   enemyMonsterImg.src = enemy.imgSrc; // CHECK THIS WITH BADE, WILL ENEMY MONSTER BE DEFINED?
-=======
-  enemyMonsterImg.src = enemyMonster.imgSrc; // CHECK THIS WITH BADE, WILL ENEMY MONSTER BE DEFINED?
->>>>>>> 4690e26bf0e45419497dc11e82b410f5d81c4561
   enemyMonsterImg.height = 80;
   enemyTarget.appendChild(enemyMonsterImg);
 }
